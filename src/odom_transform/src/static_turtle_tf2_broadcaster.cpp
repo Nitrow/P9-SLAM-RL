447bd00add2a0ac267e77dd9fc8e8d3398b8f952
Nitrow <printz.a91@gmail.com>
2020-10-10 11:25:59 +0200

    attempt at making odom_tf_transform

---
+++ src/odom_transform/src/static_turtle_tf2_broadcaster.cpp
@@ -0,0 +1,36 @@
+#include <ros/ros.h>
+#include <tf2_ros/static_transform_broadcaster.h>
+#include <tf/tf.h>
+#include <geometry_msgs/Twist.h>
+#include <nav_msgs/Odometry.h>
+
+
+void chatterCallback(const nav_msgs::Odometry::ConstPtr& msg){
+
+    static tf2_ros::StaticTransformBroadcaster static_broadcaster;
+    geometry_msgs::TransformStamped static_transformStamped;
+
+    static_transformStamped.header.stamp = ros::Time::now();
+    static_transformStamped.header.frame_id = "vehicle_blue/odom";
+    static_transformStamped.child_frame_id = "base_link";
+    static_transformStamped.transform.translation.x = msg->pose.pose.position.x;
+    static_transformStamped.transform.translation.y = msg->pose.pose.position.y;
+    static_transformStamped.transform.translation.z = msg->pose.pose.position.z;
+
+    static_transformStamped.transform.rotation.x = msg->pose.pose.orientation.x;
+    static_transformStamped.transform.rotation.y = msg->pose.pose.orientation.y;
+    static_transformStamped.transform.rotation.z = msg->pose.pose.orientation.z;
+    static_transformStamped.transform.rotation.w = msg->pose.pose.orientation.w;
+    static_broadcaster.sendTransform(static_transformStamped);
+}
+
+int main(int argc, char **argv){
+
+    ros::init(argc, argv, "odom_tf_transform");
+	ros::NodeHandle n;//create a handle node
+
+	ros::Subscriber sub = n.subscribe("/model/vehicle_blue/odometry",1000, chatterCallback);
+        //subscriber lister the msgs of given topic
+        ros::spin();//run it until you press ctrl+c
+	return 0;
+}
