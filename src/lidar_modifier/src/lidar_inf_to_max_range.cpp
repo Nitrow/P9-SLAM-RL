63451811997467584484b2c0a9d861e329c79777
Nitrow <printz.a91@gmail.com>
2020-10-11 18:37:59 +0200

    changed the maxURange a bit, so it doesn't remove object it can't see

--- src/lidar_modifier/src/lidar_inf_to_max_range.cpp
+++ src/lidar_modifier/src/lidar_inf_to_max_range.cpp
@@ -33,12 +33,12 @@ private:
 
         for (int i = 0; i < size; ++i) {
 
-            if (scan->ranges[i] < 10) {
+            if (scan->ranges[i] < scan->range_max) {
                 ranges[i] = scan->ranges[i];
 
             }
             else {
-                ranges[i] = 10;
+                ranges[i] = scan->range_max;
             }
 
             intensities[i] = scan->intensities[i];
@@ -48,14 +48,15 @@ private:
         ros::Time scan_time = ros::Time::now();
 
         // Create a LaserScan Message
-        scanmodi.header.stamp = scan_time;
-        scanmodi.header.frame_id = "vehicle_blue/lidar/gpu_lidar";
-        scanmodi.angle_min = -3.14;
-        scanmodi.angle_max = 3.14;
-        scanmodi.angle_increment = 0.009827855974435806;
-        scanmodi.time_increment = 0.0;
-        scanmodi.range_min = 0.08;
-        scanmodi.range_max = 10;
+        scanmodi.scan_time = scan->scan_time;
+        scanmodi.header.stamp = scan->header.stamp;
+        scanmodi.header.frame_id = scan->header.frame_id;
+        scanmodi.angle_min = scan->angle_min;
+        scanmodi.angle_max = scan->angle_max;
+        scanmodi.angle_increment = scan->angle_increment;
+        scanmodi.time_increment = scan->time_increment;
+        scanmodi.range_min = scan->range_min;
+        scanmodi.range_max = scan->range_max;
 
         scanmodi.ranges.resize(size);
         scanmodi.intensities.resize(size);
@@ -84,7 +85,7 @@ int main(int argc, char **argv)
 
     lidar_modifier_class::lidar_modifier_class()
     {
-        lasersub = n.subscribe("/lidar", 1, &lidar_modifier_class::laser_msg_Callback, this);
+        lasersub = n.subscribe("/lidar", 50, &lidar_modifier_class::laser_msg_Callback, this);
         lidar_pub = n.advertise<sensor_msgs::LaserScan>("/scan",50);
 
     }
