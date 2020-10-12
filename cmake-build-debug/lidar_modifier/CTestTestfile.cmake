619810db7e9b214a6af8da6aff907b22f2e7ed2e
Nitrow <printz.a91@gmail.com>
2020-10-12 09:02:16 +0200

    cleaned up

--- cmake-build-debug/lidar_modifier/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/asger/P9RL/src/lidar_modifier
-# Build directory: /home/asger/P9RL/src/cmake-build-debug/lidar_modifier
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- cmake-build-debug/simulations/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/asger/P9RL/src/simulations
-# Build directory: /home/asger/P9RL/src/cmake-build-debug/simulations
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
