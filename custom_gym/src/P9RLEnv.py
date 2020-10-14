bd105caf897bf94a7dc5c4aaddbcd0b769d4cf11
Nitrow <printz.a91@gmail.com>
2020-10-14 15:38:09 +0200

    Made some more work on custom env

--- custom_gym/src/P9RLEnv.py
+++ custom_gym/src/P9RLEnv.py
@@ -11,37 +11,40 @@ from geometry_msgs.msg import Twist
 class P9RLEnv(gym.Env):
 
     def __init__(self):
+        rospy.init_node('RLEnv', anonymous=True)
+        self.pubAction = Twist()
+        self.state = []
+        self.action = [0, 0]
+        self.reward = 0
+        self.done = False
+        self.reward = 0
         self.sub = rospy.Subscriber("/map", OccupancyGrid, self.callback)
         self.pub = rospy.Publisher('action', Twist, queue_size=10)
-        self.state = []
-        self.mapdata = []
-        self.maxAngSpeed = 10  # 2.84 max
-        self.maxLinSpeed = 10  # 0.22 max
+        self.maxAngSpeed = 10
+        self.maxLinSpeed = 10
 
         self.action_space = spaces.Box(low=np.array([-self.maxLinSpeed, -self.maxAngSpeed]),
-                                       high=np.array([0, self.maxAngSpeed]), dtype=np.float16)
+                                       high=np.array([self.maxLinSpeed, self.maxAngSpeed]), dtype=np.float16)
         self.observation_space = spaces.Box(low=-1, high=100, shape=(100000,), dtype=np.int8)
+        rospy.spin()
 
     def callback(self, data):
-        self.mapdata = data.data
-
-    def listener(self):
-        rospy.init_node('RLenv', anonymous=True)
-
-        # spin() simply keeps python from exiting until this node is stopped
-        rospy.spin()
+        self.state = data.data
 
     def reset(self):
         # RESET ENVIRONMENT
-        self.state = self.mapdata
-
         return np.asarray(self.state)
 
     def step(self, action):
-        test2 = 2
-        self.pub.publish(action)
+        self.action = action
+        self.pubAction.linear.x = self.action[0]
+        self.pubAction.angular.z = self.action[1]
+        self.pub.publish(self.pubAction.linear.x, self.pubAction.angular.z)
         # TAKE STEP
-        self.state = self.mapdata
         # GET REWARD
+        self.reward = self.setReward()
 
         return [self.state, self.reward, self.done]
+
+    def setReward(self):
+        return self.reward, self.done
