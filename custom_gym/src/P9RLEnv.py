4c729c955ce8ee52bfcc5a0bacab47aad8c1d0ab
Nitrow <printz.a91@gmail.com>
2020-10-15 01:13:21 +0200

    Worked a bit more on custom env

--- custom_gym/src/P9RLEnv.py
+++ custom_gym/src/P9RLEnv.py
@@ -20,10 +20,10 @@ class P9RLEnv(gym.Env):
         self.reward = 0
         self.sub = rospy.Subscriber("/map", OccupancyGrid, self.callback)
         self.pub = rospy.Publisher('action', Twist, queue_size=10)
-        self.maxAngSpeed = 10
-        self.maxLinSpeed = 10
+        self.maxAngSpeed = 1
+        self.maxLinSpeed = 1
 
-        self.action_space = spaces.Box(low=np.array([-self.maxLinSpeed, -self.maxAngSpeed]),
+        self.action_space = spaces.Box(low=np.array([0, -self.maxAngSpeed]),
                                        high=np.array([self.maxLinSpeed, self.maxAngSpeed]), dtype=np.float16)
         self.observation_space = spaces.Box(low=-1, high=100, shape=(100000,), dtype=np.int8)
         rospy.spin()
@@ -33,6 +33,7 @@ class P9RLEnv(gym.Env):
 
     def reset(self):
         # RESET ENVIRONMENT
+
         return np.asarray(self.state)
 
     def step(self, action):
@@ -41,10 +42,15 @@ class P9RLEnv(gym.Env):
         self.pubAction.angular.z = self.action[1]
         self.pub.publish(self.pubAction.linear.x, self.pubAction.angular.z)
         # TAKE STEP
+        # some how send a signal to step the simulation in ignition
         # GET REWARD
         self.reward = self.setReward()
 
         return [self.state, self.reward, self.done]
 
     def setReward(self):
+        self.reward = np.sum(a=self.state, axis=0, dtype=np.int8)
         return self.reward, self.done
+
+    def render(self, mode='human'):
+        pass
