04e6e4a063797194d300a320b9217a0b02da7514
Nitrow <printz.a91@gmail.com>
2020-10-18 02:09:04 +0200

    worked a bit more on gym env

--- custom_gym/src/P9RLEnv.py
+++ custom_gym/src/P9RLEnv.py
@@ -6,6 +6,7 @@ import numpy as np
 
 from nav_msgs.msg import OccupancyGrid
 from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
 
 
 class P9RLEnv(gym.Env):
@@ -13,44 +14,56 @@ class P9RLEnv(gym.Env):
     def __init__(self):
         rospy.init_node('RLEnv', anonymous=True)
         self.pubAction = Twist()
-        self.state = []
-        self.action = [0, 0]
-        self.reward = 0
-        self.done = False
-        self.reward = 0
-        self.sub = rospy.Subscriber("/map", OccupancyGrid, self.callback)
+        self.action = []
         self.pub = rospy.Publisher('action', Twist, queue_size=10)
         self.maxAngSpeed = 1
         self.maxLinSpeed = 1
 
         self.action_space = spaces.Box(low=np.array([0, -self.maxAngSpeed]),
                                        high=np.array([self.maxLinSpeed, self.maxAngSpeed]), dtype=np.float16)
-        self.observation_space = spaces.MultiDiscrete([(0, 1000), (0, 1000)])
+        self.observation_space = spaces.MultiDiscrete([(0, 1000), (0, 1000)]), spaces.Box(low=-10, high=10,
+                                                                                          shape=(360,),
+                                                                                          dtype=np.float16)
         rospy.spin()
-
-    def callback(self, data):
-        self.state = data.data
 
     def reset(self):
         # RESET ENVIRONMENT
-
-        return np.asarray(self.state)
+        state = 1
+        return state
 
     def step(self, action):
         self.action = action
         self.pubAction.linear.x = self.action[0]
         self.pubAction.angular.z = self.action[1]
-        self.pub.publish(self.pubAction.linear.x, self.pubAction.angular.z)
-        # TAKE STEP
-        # some how send a signal to step the simulation in ignition
-        # GET REWARD
-        self.reward = self.setReward()
+        self.pub.publish(self.pubAction)
+        # UNPAUSE PHYSICS
+        data = None
+        data2 = None
+        while data and data2 is None:
+            try:
+                data = rospy.wait_for_message("/map", OccupancyGrid, timeout=5)
+                data2 = rospy.wait_for_message("/lidar", LaserScan, timeout=5)
+            except:
+                pass
+        # Pause physics
 
-        return [self.state, self.reward, self.done]
+        # Get reward
+        state, done = self.setStateAndDone(data, data2)
+        reward = self.setReward(state, done)
 
-    def setReward(self):
-        self.reward = np.sum(a=self.state, axis=0, dtype=np.int8)
-        return self.reward, self.done
+        return [state, reward, done]
+
+    def setReward(self, state, done):
+        reward = np.sum(a=state, axis=0, dtype=np.int8)
+        if done:
+            reward += 100
+        return reward
 
     def render(self, mode='human'):
         pass
+
+    def setStateAndDone(self, data, data2):
+        state = data
+        done = 1
+        # Set state and done
+        return state, done
